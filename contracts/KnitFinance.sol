b312562c2dfef9cde8b1d2cc05a54971f816db40
sibizulu <sibiraj@lightrains.com>
2021-08-30 22:25:53 +0530

    Adding token

--- contracts/KnitFinance.sol
+++ contracts/KnitFinance.sol
@@ -3,10 +3,11 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract KnitFinance is ERC20 {
+contract KnitFinance is ERC20, Ownable {
 
     constructor(uint256 initialSupply) ERC20("Knit Finance", "KFT") {
-        _mint(msg.sender, initialSupply );
+        _mint(msg.sender, initialSupply);
     }
 }
--- migrations/2_token.js
+++ migrations/2_token.js
@@ -2,8 +2,8 @@ const Migrations = artifacts.require('KnitFinance')
 const Web3 = require('web3')
 require('dotenv').config()
 
-let web3 = new Web3(`https://kovan.infura.io/v3/${process.env.INFURA_KEY}`)
+let web3 = new Web3(`https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`)
 
-module.exports = function(deployer) {
-    deployer.deploy(Migrations, web3.utils.toWei('100000000', 'ether'))
+module.exports = function (deployer) {
+  deployer.deploy(Migrations, web3.utils.toWei('100000000', 'ether'))
 }
